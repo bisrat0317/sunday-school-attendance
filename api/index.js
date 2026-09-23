64a4c5bf9c3922d9049c8a9687770d0f909b16fe
Bisrat Mulatu <bisratmulatu00@gmail.com>
2026-09-23 11:55:56 +0300

    Auto-create database tables on serverless startup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const app = require('../src/server');
+const initDatabase = require('../src/scripts/initDb');
 
-module.exports = app;
+let isInitialized = false;
 
+module.exports = async (req, res) => {
+  if (!isInitialized) {
+    try {
+      await initDatabase();
+      isInitialized = true;
+    } catch (e) {
+      console.error('Serverless database auto-initialization error:', e);
+    }
+  }
+  return app(req, res);
+};
